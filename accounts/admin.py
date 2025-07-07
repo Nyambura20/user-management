a8eac389e1a2d6a5a221f721ba986089636965e7
BSIT606J2022 <116421244+BSIT606J2022@users.noreply.github.com>
2025-07-07 15:19:34 +0300

    admin features: added an admin panel and restrict normal users from viewing and accessing admin features

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_verified', 'is_staff', 'date_joined')
+    list_filter = ('is_verified', 'is_staff', 'date_joined')
+    search_fields = ('username', 'email')
